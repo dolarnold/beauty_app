2afbb4e3b39626e06a3b80961dbc86f756d3f314
dolarnold <34762964+dolarnold@users.noreply.github.com>
2020-04-09 01:14:09 +0300

    working on the grid view on the home page

---
+++ lib/components/products.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+//my own imports
+import 'package:mremboco/components/horizontal_listview.dart';
+import 'package:mremboco/components/products.dart.';
+
+class Products extends StatefulWidget {
+  @override
+  _ProductsState createState() => _ProductsState();
+}
+
+class _ProductsState extends State<Products> {
+  var product_list = [
+    {
+      "name":,
+      "picture":,
+      "old_price":,
+      "price",
+  }
+
+  ]
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
