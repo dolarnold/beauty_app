aa85d02d00dd30da1781f33678b1dca2e8d6c16c
dolarnold <34762964+dolarnold@users.noreply.github.com>
2020-04-09 15:58:03 +0300

    product list page done

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -40,19 +40,122 @@ class _ProductDetailsState extends State<ProductDetails> {
               onPressed: () {})
         ],
       ),
-      body: new ListView(
-        children: <Widget>[
-          new Container(
-            height: 300.0,
-            child:GridTile(
-              child: Container(
-                color: Colors.white,
-                child:Image.asset(widget.product_details_picture),
+      body: new ListView(children: <Widget>[
+        new Container(
+          height: 300.0,
+          child: GridTile(
+            child: Container(
+              color: Colors.white,
+              child: Image.asset(widget.product_details_picture),
+            ),
+            footer: new Container(
+              color: Colors.white70,
+              child: ListTile(
+                leading: new Text(widget.product_details_name,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
+                title: new Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: new Text(
+                        "\$${widget.product_details_old_price}",
+                        style: TextStyle(
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough),
+                      ),
+                    ),
+                    Expanded(
+                      child: new Text(
+                        "\$${widget.product_details_new_price}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          )
-        ]
-      ),
+          ),
+        ),
+        // ============THE FIRST BUTTON==================
+        Row(
+          children: <Widget>[
+            //===========SIZE BUTTONS==================
+            Expanded(
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.white,
+                textColor: Colors.grey,
+                elevation: 0.2,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(child: new Text("Size")),
+                    Expanded(child: new Icon(Icons.arrow_drop_down)),
+                  ],
+                ),
+              ),
+            ),
+
+            // ======Size buttons======
+            Expanded(
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.white,
+                textColor: Colors.grey,
+                elevation: 0.2,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(child: new Text("Color")),
+                    Expanded(child: new Icon(Icons.arrow_drop_down)),
+                  ],
+                ),
+              ),
+            ),
+            // =================
+            Expanded(
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.white,
+                textColor: Colors.grey,
+                elevation: 0.2,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(child: new Text("Qty")),
+                    Expanded(child: new Icon(Icons.arrow_drop_down)),
+                  ],
+                ),
+
+
+              ),
+            ),
+          ],
+        ),
+        // ============THE Second BUTTON==================
+        Row(
+          children: <Widget>[
+
+            // ======Size buttons======
+
+            // =================
+            Expanded(
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.red,
+                textColor: Colors.grey,
+                elevation: 0.2,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(child: new Text("buy Now")),
+                  ],
+                ),
+
+              ),
+            ),
+            new IconButton(icon:Icon(Icons.shopping_cart), color: Colors.red,onPressed: (){}),
+            new IconButton(icon:Icon(Icons.favorite_border),color: Colors.red, onPressed: (){}),
+          ],
+        ),
+      ]),
     );
   }
 }
